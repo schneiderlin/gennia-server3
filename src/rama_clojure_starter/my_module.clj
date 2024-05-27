ab938dccc1708f5658d883376861ecb11c66dd97
linzihao <positivevector@gmail.com>
2024-05-27 16:45:11 +0800

    word count

--- src/rama_clojure_starter/my_module.clj
+++ src/rama_clojure_starter/my_module.clj
@@ -24,4 +24,53 @@
 
 (def ipc (rtest/create-ipc))
 
+(rtest/launch-module! ipc WordCountModule {:tasks 4 :threads 2})
+
+(def sentences-depot (foreign-depot ipc (get-module-name WordCountModule) "*sentences-depot"))
+(def word-counts (foreign-pstate ipc (get-module-name WordCountModule) "$$word-counts"))
+
+(comment
+  (foreign-append! sentences-depot "Hello world")
+  (foreign-append! sentences-depot "hello hello goodbye")
+  (foreign-append! sentences-depot "Alice says hello")
+
+  (foreign-select-one (keypath "hello") word-counts) ; => 4
+  (foreign-select-one (keypath "goodbye") word-counts) ; => 1
+
+  (close! ipc)
+  :rcf)
+
+;; 在 module 以外也可以调用 dataflow 的各种 api, partitioner 除外
+(comment
+  (?<-
+   (println "Hello, world!"))
+
+  (deframaop foo [*a]
+    (:> (inc *a))
+    (:> (dec *a)))
+  
+  (?<-
+   (foo 5 :> *v)
+   (println *v))
+  
+  ;; <<if 看成是 if 就可以了
+  ;; else> 看成是 else
+  (?<-
+   (<<if (= 1 2)
+         (println "true branch 1")
+         (println "true branch 2")
+         (else>)
+         (println "else branch 1")
+         (println "else branch 2")))
+  
+  (deframaop my-filter [*v]
+    (<<if *v
+          (:>)))
+  
+  (?<-
+   (ops/range> 0 5 :> *v)
+   (my-filter (even? *v))
+   (println *v))
+
+  :rcf)
 
