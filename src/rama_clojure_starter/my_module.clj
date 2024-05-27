fd83ccb97184b1d934de4d281a9c27effd07ee96
linzihao <670894261@qq.com>
2024-05-27 15:06:31 +0800

    work count

--- src/rama_clojure_starter/my_module.clj
+++ src/rama_clojure_starter/my_module.clj
@@ -1,8 +1,27 @@
 (ns rama-clojure-starter.my-module
   (:use [com.rpl.rama]
         [com.rpl.rama.path])
-  (:require [com.rpl.rama.aggs :as aggs]
-            [com.rpl.rama.ops :as ops]))
+  (:require
+   [clojure.string :as str]
+   [com.rpl.rama.test :as rtest]
+   [com.rpl.rama.aggs :as aggs]
+   [com.rpl.rama.ops :as ops]))
 
-(defmodule MyModule [setup topologies]
-  )
+;; *something means variable in Rama dataflow
+;; <<sources just like a datalow block macro, no specific semantic
+;; source> subscribe to depot
+
+(defmodule WordCountModule [setup topologies]
+  (declare-depot setup *sentences-depot :random)
+  (let [s (stream-topology topologies "word-count")]
+    (declare-pstate s $$word-counts {String Long})
+    (<<sources s
+               (source> *sentences-depot :> *sentence)
+               (str/split (str/lower-case *sentence) #" " :> *words)
+               (ops/explode *words :> *word)
+               (|hash *word)
+               (+compound $$word-counts {*word (aggs/+count)}))))
+
+(def ipc (rtest/create-ipc))
+
+
